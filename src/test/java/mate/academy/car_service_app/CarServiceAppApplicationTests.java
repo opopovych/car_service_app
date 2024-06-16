e94e8def2f7f13c05051f8c5654bbe3d00f04708
Oleh <popovich158@gmail.com>
2024-06-16 23:33:14 +0300

    Created empty Spring project.

---
+++ src/test/java/mate/academy/car_service_app/CarServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.car_service_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
